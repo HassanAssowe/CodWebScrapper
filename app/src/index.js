22cbb76301fa9e16d0ba61f992d77dd682804c90
h3khaira <h3khaira@uwaterloo.ca>
2020-05-08 17:31:40 -0400

    converted from functional component to class

--- app/src/index.js
+++ app/src/index.js
@@ -1,18 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/app/App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, FormGroup, FormControl } from "react-bootstrap";
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: '',
+      loginStatus: '',
+    }
+    this.validateForm = this.validateForm.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0
+  };
+  
+  handleSubmit(event) {
+  event.preventDefault();
+  fetch('/login?email=' + this.state.email + '&password=' + this.state.password)
+    .then(res => res.json())
+    .then(data => { this.setState({ loginStatus: data }) })
+  };
+
+  render() {
+    return (
+      <div className="login">
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="email">
+            <FormControl
+              autoFocus
+              type="email"
+              placeholder="Enter Email"
+              value={this.state.email}
+              onChange={e => this.setState({ email: e.target.value })}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" >
+            <FormControl
+              value={this.state.password}
+              placeholder="Enter Password"
+              onChange={e => this.setState({ password: e.target.value })}
+              type="password"
+            />
+          </FormGroup>
+          <Button block disabled={!this.validateForm()} type="submit">
+            Login
+          </Button>
+        </form>
+        {this.state.loginStatus === "Fail" && <p id="loginError">Wrong Credentials</p>}
+      </div>
+    )
+  }
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
