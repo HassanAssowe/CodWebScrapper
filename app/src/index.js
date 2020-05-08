328baddcf907ef0c408cdecc62752245c39d6d6a
h3khaira <h3khaira@uwaterloo.ca>
2020-05-08 18:20:42 -0400

    created transition from login page

--- app/src/index.js
+++ app/src/index.js
@@ -12,14 +12,21 @@ class Login extends React.Component {
       email: '',
       password: '',
       loginStatus: '',
+      username: '',
     }
     this.validateForm = this.validateForm.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.validateUser = this.validateUser.bind(this)
+    this.handleUsername =this.handleUsername.bind(this)
   }
 
   validateForm() {
     return this.state.email.length > 0 && this.state.password.length > 0
   };
+
+  validateUser() {
+    return this.state.username.length>0;
+  }
   
   handleSubmit(event) {
   event.preventDefault();
@@ -28,34 +35,60 @@ class Login extends React.Component {
     .then(data => { this.setState({ loginStatus: data }) })
   };
 
+  handleUsername(event) {
+    event.preventDefault();
+    fetch('/user?username='+this.state.username)
+    .then(res => res.json())
+    .then(data => {console.log(data)})
+  }
   render() {
-    return (
-      <div className="login">
-        <form onSubmit={this.handleSubmit}>
-          <FormGroup controlId="email">
-            <FormControl
+    if (this.state.loginStatus !== "Success") {
+      return (
+        <div className="login">
+          <form onSubmit={this.handleSubmit}>
+            <FormGroup controlId="email">
+              <FormControl
+                autoFocus
+                type="email"
+                placeholder="Enter Email"
+                value={this.state.email}
+                onChange={e => this.setState({ email: e.target.value })}
+              />
+            </FormGroup>
+            <FormGroup controlId="password" >
+              <FormControl
+                value={this.state.password}
+                placeholder="Enter Password"
+                onChange={e => this.setState({ password: e.target.value })}
+                type="password"
+              />
+            </FormGroup>
+            <Button block disabled={!this.validateForm()} type="submit">
+              Login
+            </Button>
+          </form>
+          {this.state.loginStatus === "Fail" && <p id="loginError">Unable to Verify Credentials</p>}
+        </div>
+      )
+    } else {
+      return(
+        <div className="user-info">
+          <form onSumbit={this.handleUsername}>
+            <FormGroup controlId = "username">
+              <FormControl
               autoFocus
-              type="email"
-              placeholder="Enter Email"
-              value={this.state.email}
-              onChange={e => this.setState({ email: e.target.value })}
-            />
-          </FormGroup>
-          <FormGroup controlId="password" >
-            <FormControl
-              value={this.state.password}
-              placeholder="Enter Password"
-              onChange={e => this.setState({ password: e.target.value })}
-              type="password"
-            />
-          </FormGroup>
-          <Button block disabled={!this.validateForm()} type="submit">
-            Login
-          </Button>
-        </form>
-        {this.state.loginStatus === "Fail" && <p id="loginError">Wrong Credentials</p>}
-      </div>
-    )
+              placeholder="Enter Username"
+              value={this.state.username}
+              onChange={e => this.setState({username: e.target.value})}
+              />
+            </FormGroup>
+            <Button block disabled={!this.validateUser()} type="submit">
+              Submit
+            </Button>
+          </form>
+        </div>
+      )
+    } 
   }
 };
 
